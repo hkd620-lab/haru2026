689bf00353f251f1576db11c21aa9be8ce543584
경대 허 <hkd620@gmail.com>
2026-02-22 09:22:11 +0900

    보안 점검 - 불필요한 설정 파일 및 Supabase 폴더 제거

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
- apiKey: "AIzaSyAfX4Chb3wYewrFmbLHCb2yqbmbWPUEEgQ",
-  authDomain: "haru2026-8abb8.firebaseapp.com",
-  projectId: "haru2026-8abb8",
-  storageBucket: "haru2026-8abb8.firebasestorage.app",
-  messagingSenderId: "398138267500",
-  appId: "1:398138267500:web:7009366654c6ecea828549",
-};
